7caafd4b9dc33a0548dbdda385eada0b90c389de
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-03-22 21:28:11 +0500

    Remove obsolete permissions module and simplify auth client configuration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,18 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 import { adminClient } from "better-auth/client/plugins";
 
-import { ac, adminRole, userRole, vendorRole } from "~/lib/permissions";
-
 export const auth = createAuthClient({
   baseURL: "http://localhost:8080",
-  plugins: [
-    adminClient({
-      ac,
-      roles: {
-        adminRole,
-        vendorRole,
-        userRole,
-      },
-    }),
-  ],
+  plugins: [adminClient()],
 });

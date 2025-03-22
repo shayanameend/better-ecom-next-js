cac53c2e500cc9ab062a9fd8a611cefbc9016ac1
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-03-22 09:50:11 +0500

    Replace Sign Out button with LoadingButton for improved user feedback during sign-out process; integrate toast notifications for success and error handling.

--- src/components/layout/headers/header-cta.tsx
+++ src/components/layout/headers/header-cta.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import Link from "next/link";
+import { useState } from "react";
+
+import { toast } from "sonner";
 
 import { auth } from "~/auth/client";
-import { Button, buttonVariants } from "~/components/ui/button";
+import { Button, buttonVariants, LoadingButton } from "~/components/ui/button";
 import { routes } from "~/lib/routes";
 import { cn } from "~/lib/utils";
 
 export function HeaderCTA() {
+  const [isLoading, setIsLoading] = useState(false);
+
   const { data, isPending, error } = auth.useSession();
 
   if (isPending) {
@@ -21,13 +26,26 @@ export function HeaderCTA() {
   return (
     <>
       {data ? (
-        <Button
+        <LoadingButton
+          isLoading={isLoading}
           onClick={() => {
-            auth.signOut();
+            auth.signOut({
+              fetchOptions: {
+                onRequest: (ctx) => {
+                  setIsLoading(true);
+                },
+                onSuccess: (ctx) => {
+                  toast.success("Signed out successfully");
+                },
+                onError: (ctx) => {
+                  toast.error(ctx.error.message);
+                },
+              },
+            });
           }}
         >
           Sign Out
-        </Button>
+        </LoadingButton>
       ) : (
         <Link
           href={routes.app.auth.signIn.url()}
